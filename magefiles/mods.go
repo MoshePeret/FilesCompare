7b1f3fefe0843e6c6707a8626b08861629e99064
Your Name <you@example.com>
2024-02-16 16:31:15 +0200

    add offline dev env

---
+++ magefiles/mods.go
@@ -0,0 +1,27 @@
+package magefiles
+
+import (
+	_ "github.com/bramvdbogaerde/go-scp"
+	_ "github.com/clbanning/mxj/v2"
+	_ "github.com/fsnotify/fsnotify"
+	_ "github.com/hashicorp/hcl"
+	_ "github.com/inconshreveable/mousetrap"
+	_ "github.com/magiconair/properties"
+	_ "github.com/mitchellh/go-homedir"
+	_ "github.com/mitchellh/mapstructure"
+	_ "github.com/pelletier/go-toml/v2"    // indirect
+	_ "github.com/sagikazarmark/locafero"  // indirect
+	_ "github.com/sagikazarmark/slog-shim" // indirect
+	_ "github.com/sourcegraph/conc"        // indirect
+	_ "github.com/spf13/afero"             // indirect
+	_ "github.com/spf13/cast"              // indirect
+	_ "github.com/spf13/cobra"             // indirect
+	_ "github.com/spf13/pflag"             // indirect
+	_ "github.com/spf13/viper"             // indirect
+	_ "github.com/subosito/gotenv"         // indirect
+	_ "go.uber.org/atomic"                 // indirect
+	_ "go.uber.org/multierr"               // indirect
+	_ "golang.org/x/text"                  // indirect
+	_ "gopkg.in/ini.v1"                    // indirect
+	_ "gopkg.in/yaml.v3"                   // indirect
+)
